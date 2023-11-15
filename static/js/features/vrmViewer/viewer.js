a8d6f0e18e77509aca40a1879e7f2a840b40a7e3
Harper Reed <harper@nata2.org>
2023-11-15 09:03:24 -0600

    zoomed out in default view

--- static/js/features/vrmViewer/viewer.js
+++ static/js/features/vrmViewer/viewer.js
@@ -77,7 +77,8 @@ export class Viewer {
 
     // camera
     this._camera = new THREE.PerspectiveCamera(20.0, width / height, 0.1, 20.0);
-    this._camera.position.set(0, 1.3, 1.5);
+    // this._camera.position.set(0, 1.3, 1.5);
+    this._camera.position.set(0, 1.3, 12);
     this._cameraControls?.target.set(0, 1.3, 0);
     this._cameraControls?.update();
     // camera controls
